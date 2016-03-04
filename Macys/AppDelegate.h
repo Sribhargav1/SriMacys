2e2066062eec5a7e34941773f080c5afc7e644fd
Venkata Nandanavanam <venkatanandanavanam@venkatas-macbook-pro.local>
2016-03-03 23:49:58 -0500

    Initial Commit

---
+++ Macys/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Macys
+//
+//  Created by Venkata  Nandanavanam on 3/3/16.
+//  Copyright © 2016 Venkata  Nandanavanam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Macys/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Macys
+//
+//  Created by Venkata  Nandanavanam on 3/3/16.
+//  Copyright © 2016 Venkata  Nandanavanam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
